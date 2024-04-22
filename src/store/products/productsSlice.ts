617b66d5a26c5bf4515a03cd0590928c66562d37
kalpesh-coditas <kalpesh.rane@coditas.com>
2024-04-22 10:08:55 +0530

    added filters

--- src/store/products/productsSlice.ts
+++ src/store/products/productsSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { getProducts } from '../../services/productService';
-import { ProductsState } from './IProducts';
+import { ProductData, ProductsState } from './IProducts';
 
 
 const initialState: ProductsState = {
@@ -10,6 +10,13 @@ const initialState: ProductsState = {
     error: null,
     isLoading: false,
     total: 2,
+
+    filters: {
+        category: "",
+        sortByPrice: "ASC",
+        sortByPopularity: "ASC",
+        searchBy: ""
+    },
 }
 
 export const productsSlice = createSlice({
@@ -19,10 +26,54 @@ export const productsSlice = createSlice({
         setData(state, action) {
             state.productData = action.payload.data.products;
             state.total = action.payload.data.total;
+        },
+        setInbuiltFilters(state, action) {
+            state.productData = [];
+            state.filters = { ...state.filters, [action.payload.key]: action.payload.value }
+            state.total = 0;
+        },
+
+        setExternalFilters(state, action) {
+            // for filters on existing products
+            switch (action.payload) {
+                case "sortByPopularity_ASC": {
+                    state.productData = state.productData.sort((a: ProductData, b: ProductData) => {
+                        if (a.rating < b.rating) return -1;
+                        if (a.rating > b.rating) return 1;
+                        return 0;
+                    })
+                    break;
+                };
+                case "sortByPopularity_DSC": {
+                    state.productData = state.productData.sort((a: ProductData, b: ProductData) => {
+                        if (a.rating > b.rating) return -1;
+                        if (a.rating < b.rating) return 1;
+                        return 0;
+                    })
+                    break;
+                };
+                case "sortByPrice_ASC": {
+                    state.productData = state.productData.sort((a: ProductData, b: ProductData) => {
+                        if (a.price < b.price) return -1;
+                        if (a.price > b.price) return 1;
+                        return 0;
+                    })
+                    break;
+                };
+                case "sortByPrice_DSC": {
+                    state.productData = state.productData.sort((a: ProductData, b: ProductData) => {
+                        if (a.price > b.price) return -1;
+                        if (a.price < b.price) return 1;
+                        return 0;
+                    })
+                    break;
+                }
+            }
         }
+
     },
     extraReducers(builder) {
-        builder.addCase(getProducts.pending, (state, action) => {
+        builder.addCase(getProducts.pending, (state) => {
             state.isLoading = true
         }).addCase(getProducts.fulfilled, (state, action) => {
             state.isLoading = false;
@@ -33,6 +84,6 @@ export const productsSlice = createSlice({
     }
 })
 
-export const { setData } = productsSlice.actions
+export const { setData, setInbuiltFilters, setExternalFilters } = productsSlice.actions
 
 export default productsSlice.reducer
